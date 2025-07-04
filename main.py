e07ae4a9c029cf4788a536de0060936141f8b8cd
Alexandros Antoniou <hayiate4@hotmail.com>
2025-07-04 12:07:03 +0300

    Add risk metrics API endpoints and requirements file
    - Implemented a new endpoint to calculate risk metrics from a list of prices.
    - Added a second endpoint to fetch and calculate risk metrics from a stock ticker using yfinance.
    - Introduced a requirements.txt file to manage dependencies including FastAPI, Uvicorn, Pandas, NumPy, and yfinance.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import pandas as pd
+import yfinance as yf
+import uvicorn
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import pandas as pd
 
 app = FastAPI()
 
 
 class PriceInput(BaseModel):
     prices: list[float]
+
+
+class TickerInput(BaseModel):
+    ticker: str
 
 
 @app.post("/risk_metrics")
@@ -28,6 +34,41 @@ def calculate_risk(data: PriceInput):
         "volatility": volatility,
         "sharpe_ratio": sharpe_ratio,
         "max_drawdown": max_drawdown
+    }
+
+
+@app.post("/risk_metrics_from_ticker")
+def risk_from_ticker(input: TickerInput):
+    ticker = input.ticker.upper()
+    try:
+        df = yf.download(ticker, period="6mo")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    if df.empty:
+        raise HTTPException(
+            status_code=404, detail="No data found for this ticker")
+
+    prices = df["Close"].dropna()
+
+    if len(prices) < 2:
+        raise HTTPException(
+            status_code=400, detail="At least 2 prices are required")
+
+    returns = prices.pct_change().dropna()
+    mean_return = returns.mean()
+    volatility = returns.std()
+    sharpe_ratio = mean_return / volatility
+
+    drawdown = (prices / prices.cummax())-1
+    max_drawdown = drawdown.min()
+
+    return {
+        "ticker": ticker,
+        "mean_return": round(mean_return, 4),
+        "volatility": round(volatility, 4),
+        "sharpe_ratio": round(sharpe_ratio, 2),
+        "max_drawdown": round(max_drawdown, 4)
     }
 
 

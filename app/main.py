8849d71fe1451116ef4d18b105a71d05ecac66a4
Alexandros Antoniou <hayiate4@hotmail.com>
2025-07-05 12:24:39 +0300

    Add environment configuration and update dependencies
    - Introduced a .env file for environment variable management, defaulting to 'dev'.
    - Updated requirements.txt to include 'python-dotenv' for loading environment variables.
    - Modified CORS middleware in main.py to dynamically set allowed origins based on the environment.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 """ Main application entry point for the risk metrics API. """
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from .routes import router
 
+load_dotenv()
+
+ENV = os.getenv("ENV", "dev")
+
+if ENV == "prod":
+    origins = ["https://risk-ui-nine.vercel.app"]
+else:
+    origins = ["http://localhost:5173"]
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

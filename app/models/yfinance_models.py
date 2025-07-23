310ec1ef54622185e10d2d30f48deb76cf2c32cc
Alexandros Antoniou <hayiate4@hotmail.com>
2025-07-23 18:06:06 +0300

    Update TickerSearchReference model and enhance search_ticker endpoint with caching
    - Modified TickerSearchReference model to make shortname, typeDisp, and exchDisp optional, improving flexibility.
    - Refactored search_ticker endpoint to implement caching with Redis, reducing redundant API calls and improving response times.
    - Added logging for invalid ticker search results to enhance debugging and monitoring capabilities.

--- app/models/yfinance_models.py
+++ app/models/yfinance_models.py
@@ -6,12 +6,12 @@ from pydantic import BaseModel
 class TickerSearchReference(BaseModel):
     """ A reference to a ticker search result. """
     symbol: str
-    shortname: str
+    shortname: Optional[str] = None
+    typeDisp: Optional[str] = None
+    exchDisp: Optional[str] = None
     exchange: Optional[str] = None
     quoteType: Optional[str] = None
     longname: Optional[str] = None
     index: Optional[str] = None
     score: Optional[float] = None
-    typeDisp: Optional[str] = None
-    exchDisp: Optional[str] = None
     isYahooFinance: Optional[bool] = None
--- app/routes/portfolio_routes.py
+++ app/routes/portfolio_routes.py
@@ -1,5 +1,6 @@
 """ Portfolio routes for the application. """
 from typing import List
+import logging
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session, select
 import yfinance as yf
@@ -13,6 +14,7 @@ from app.models.yfinance_models import TickerSearchReference
 from app.redis_service import redis_service
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 def validate_ticker(ticker):
@@ -168,18 +170,28 @@ async def delete_position(
 
 
 @router.get("/search_ticker", response_model=List[TickerSearchReference])
-def search_ticker(q: str = Query(..., min_length=1, max_length=30)):
+async def search_ticker(q: str = Query(..., min_length=1, max_length=30)):
     """ Search for tickers by name or symbol using yfinance.Search. """
+    cache_key = f"ticker_search:{q}"
+
+    cached_data = await redis_service.get_cached_data(cache_key)
+    if cached_data:
+        return cached_data
+
     try:
         data = yf.Search(q).quotes
         results = []
         for item in data:
-            if not item.get("symbol") or not item.get("shortname"):
+
+            if not item.get("symbol"):
+                logger.warning("Invalid ticker search result: %s", item)
                 continue
 
             results.append(item)
 
+        await redis_service.set_cached_data(cache_key, results[:10])
         return results[:10]
+
     except Exception as e:
         raise HTTPException(
             status_code=500, detail="Something went wrong while searching for tickers") from e

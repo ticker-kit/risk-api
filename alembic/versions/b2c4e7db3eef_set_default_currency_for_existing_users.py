5907715e08d1cf91719c5ea519a675ec1b8d96f6
Alexandros Antoniou <hayiate4@hotmail.com>
2025-08-09 21:28:18 +0300

    Add Alembic migration setup and user model updates
    - Introduced Alembic configuration files and environment setup for database migrations.
    - Added migration scripts to enhance the User model by introducing a currency field and setting default values.
    - Updated user model to reflect the new currency field with a default value of 'EUR'.
    - Modified user routes to retrieve user currency from the database instead of using hardcoded values, improving data integrity.

---
+++ alembic/versions/b2c4e7db3eef_set_default_currency_for_existing_users.py
@@ -0,0 +1,30 @@
+"""Set default currency for existing users
+
+Revision ID: b2c4e7db3eef
+Revises: 3d66c6735def
+Create Date: 2025-08-09 21:22:24.629614
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlmodel import SQLModel
+import sqlmodel
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b2c4e7db3eef'
+down_revision: Union[str, Sequence[str], None] = '3d66c6735def'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute("UPDATE user SET currency = 'EUR' WHERE currency IS NULL")
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass

e05a03ba2ca0c1895b740234d9e7d52da126bd30
Alexandros Antoniou <hayiate4@hotmail.com>
2025-09-01 23:22:53 +0300

    Refactor YFinanceService and introduce asset analysis models
    - Refactored the YFinanceService to improve ticker validation and data retrieval processes, including the introduction of an adjust_ticker method for cleaner ticker handling.
    - Added new AssetPeriod and AssetAnalysis models to encapsulate asset-related data and calculations, enhancing the structure and maintainability of the code.
    - Introduced statistical functions for fitted values to support asset analysis.
    - Updated risk routes to utilize the new asset analysis functionality, streamlining the process of retrieving and processing ticker data.

--- app/models/yfinance_models.py
+++ app/models/yfinance_models.py
@@ -1,4 +1,8 @@
-""" Yfinance models for the application. """
+""" 
+Yfinance models for the application.
+More fields can be added as needed.
+May break if yfinance changes the fields.
+"""
 from typing import Optional
 from pydantic import BaseModel
 
@@ -20,8 +24,28 @@ class TickerSearchReference(BaseModel):
 class TickerInfo(BaseModel):
     """ A model for ticker information. """
     symbol: str
+    longName: Optional[str] = None
+    shortName: str
     quoteType: str
     typeDisp: str
+    fullExchangeName: str
+    longBusinessSummary: str
+    quoteSourceName: str
     currency: str
     exchange: str
+    region: str
+
+    ask: Optional[float] = None
+    bid: Optional[float] = None
+    country: Optional[str] = None
+    beta: Optional[float] = None
     expireIsoDate: Optional[str] = None
+    industryDisp: Optional[str] = None
+    sectorDisp: Optional[str] = None
+    longBusinessSummary: Optional[str] = None
+    enterpriseValue: Optional[float] = None
+    bookValue: Optional[float] = None
+    marketCap: Optional[float] = None
+    category: Optional[str] = None
+    fundFamily: Optional[str] = None
+    regularMarketChangePercent: Optional[float] = None
